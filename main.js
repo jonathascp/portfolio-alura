b42ce311d2d8788f7520f7b3c0a14b633b9ee099
Jonathas dos Anjos <127884309+jonathascp@users.noreply.github.com>
2023-06-04 16:30:53 -0300

    ajustes finais 1/?

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
 
 function clickMenu()
 {
-    if(itens.style.display == 'none')
+    if(itens.style.display == 'block')
+    {
+        itens.style.display = 'none';
+    }
+    else
     {
         itens.style.display = 'block';
     }
-    else{
+
+}
+
+function mudouTamanho()
+{
+    if(window.innerWidth >= 768)
+    {
+        itens.style.display = 'block';
+    }
+
+    else
+    {
         itens.style.display = 'none';
     }
 }
